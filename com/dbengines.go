3cb6c9551f5f13fca95bbe09b445719d1be65a09
am <am@acorello.it>
2023-03-18 10:53:16 +0100

    first commit

---
+++ com/dbengines.go
@@ -0,0 +1,62 @@
+// gathers DBMS details from db-engines.com
+package dbengines
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type Database struct {
+	URL  string
+	Name string
+}
+
+const (
+	HostName    = "db-engines.com"
+	SystemsPage = "https://" + HostName + "/en/systems"
+)
+
+func DatabaseDetail() {
+	systemsDocument := getSystemsDocument()
+
+	linksSelection := systemsDocument.Find("table.list a")
+	databases := make([]Database, 0, linksSelection.Length())
+	linksSelection.Each(func(_ int, dbAnchor *goquery.Selection) {
+		db := dbLink(dbAnchor)
+		if db.Name == "" || db.URL == "" {
+			return
+		}
+		databases = append(databases, db)
+	})
+	for _, db := range databases {
+		fmt.Println(db.Name, db.URL)
+	}
+}
+
+func getSystemsDocument() *goquery.Document {
+	resp, err := http.Get(SystemsPage)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	systemsPage, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return systemsPage
+}
+
+func dbLink(anchor *goquery.Selection) Database {
+	anchor.Find("span").Remove()
+	href, _ := anchor.Attr("href")
+	href = strings.TrimSpace(href)
+	name := strings.TrimSpace(anchor.Text())
+	return Database{
+		URL:  href,
+		Name: name,
+	}
+}
