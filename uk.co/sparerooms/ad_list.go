ef2554c749981a863db9c4812adbd6cafe4bf42c
am <am@acorello.it>
2024-03-02 15:10:02 +0000

    [SpareRoom] Refactor PrintAdList
    Extract function to get Ad out of goquery.Document

--- uk.co/sparerooms/ad_list.go
+++ uk.co/sparerooms/ad_list.go
@@ -24,28 +24,34 @@ func PrintAdList(r io.Reader) {
 		log.Fatal(err)
 	}
 	articles := d.Find("li[class='listing-result']")
-
-	firstFigureAnchor := goquery.Single("figure > a")
-	type Ad struct {
-		Title     string
-		ListingId string
-		PostCo    string
-		Href      string
-	}
 	var ads = make([]Ad, articles.Size())
 	articles.Each(func(i int, article *goquery.Selection) {
-		href, _ := url.Parse(article.FindMatcher(firstFigureAnchor).AttrOr("href", ""))
-		q := href.Query()
-		q.Del("search_results")
-		href.RawQuery = q.Encode()
-		ads[i] = Ad{
-			Title:     article.AttrOr("data-listing-title", "N/A"),
-			ListingId: article.AttrOr("data-listing-id", "N/A"),
-			PostCo:    article.AttrOr("data-listing-postcode", "N/A"),
-			Href:      href.String(),
-		}
+		ads[i] = queryAd(article)
 	})
 	for i := range ads {
 		adTemplate.Execute(os.Stdout, ads[i])
 	}
+}
+
+type Ad struct {
+	Title     string
+	ListingId string
+	PostCo    string
+	Href      string
+}
+
+var firstFigureAnchor = goquery.Single("figure > a")
+
+func queryAd(article *goquery.Selection) Ad {
+	href, _ := url.Parse(article.FindMatcher(firstFigureAnchor).AttrOr("href", ""))
+	q := href.Query()
+	q.Del("search_results")
+	href.RawQuery = q.Encode()
+	ad := Ad{
+		Title:     article.AttrOr("data-listing-title", "N/A"),
+		ListingId: article.AttrOr("data-listing-id", "N/A"),
+		PostCo:    article.AttrOr("data-listing-postcode", "N/A"),
+		Href:      href.String(),
+	}
+	return ad
 }
