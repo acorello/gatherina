859b7b8a740106e0d127ac55d84d67222b71d801
am <am@acorello.it>
2025-06-21 12:39:54 +0100

    Drop unnecessary failing test

--- scrapers/sparerooms/main_test.go
+++ scrapers/sparerooms/main_test.go
@@ -5,16 +5,9 @@ import (
 	"testing"
 
 	"github.com/acorello/must"
-	"github.com/matryer/is"
 )
 
 func TestProcessingSample(t *testing.T) {
 	searchURL := *must.Get(url.Parse("testdata/samples/search@01.html"))
 	processURL(searchURL, PrintAdList)
-}
-
-func TestIs(t *testing.T) {
-	st := is.New(t)
-	st.Equal(1, 2) // expected equality
-	t.Error("not equal")
 }
